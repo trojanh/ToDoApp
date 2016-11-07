484ab73c3aaf7eb602183a1819d923b772836c63
trojanh <rajan.tiwari@kiprosh.com>
2016-11-07 19:47:56 +0530

    added time of todo completion instead of creation and followed recommended indentation principles

--- app/controllers/todos.js
+++ app/controllers/todos.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  
+
 	actions: {
 		createTodo() {
 			var title = this.get('newTitle');
@@ -15,60 +15,49 @@ export default Ember.Controller.extend({
 			});
 
       		//clear out text field
-      		this.set('newTitle','');
-      		todo.save();
-      	},
+      this.set('newTitle','');
+      todo.save();
+    },
 
-      	// editTodo(todo) {
-      	// 	todo.set('isEditing', true);
-      	// },
-      	// removeTodo(todo) {
-      	// 	todo.destroyRecord();
-      	// },
+    destroyAll(model) {
+    	model.invoke('deleteRecord');
+    	model.invoke('save');
+    },
 
-      	destroyAll(model) {
-      		model.invoke('deleteRecord');
-      		model.invoke('save');
-      	},
+    clearCompleted() {
+    	var completed = this.get('model').filterBy('isCompleted', true);
+    	completed.invoke('deleteRecord');
+    	completed.invoke('save');
+    }
+  },
 
-      	clearCompleted() {
-      		var completed = this.get('model').filterBy('isCompleted', true);
-      		completed.invoke('deleteRecord');
-      		completed.invoke('save');
-      	}
-      },
+  hasCompleted: Ember.computed('completed', function(){
+  	return this.get('completed') > 0;
+  }),
 
+  completed: Ember.computed('model.@each.isCompleted', function(){
+  	return this.get('model').filterBy('isCompleted', true).get('length');
+  }),
 
+  remaining: Ember.computed('model.@each.isCompleted', function(){
+  	return this.get('model').filterBy('isCompleted', false).get('length');
+  }),
 
-      hasCompleted: Ember.computed('completed', function(){
-      	return this.get('completed') > 0;
-      }),
+  inflection: Ember.computed('remaining', function(){
+  	var remaining = this.get('remaining');
+  	return remaining <= 1 ? 'item' : 'items';
+  }),
 
-      completed: Ember.computed('model.@each.isCompleted', function(){
-      	return this.get('model').filterBy('isCompleted', true).get('length');
-      }),
+  allAreDone: Ember.computed("model.@each.isCompleted", {
+    get() {
+      return this.get('model').get('length') && this.get('model').isE('isCompleted');
+  },
 
-      remaining: Ember.computed('model.@each.isCompleted', function(){
-      	return this.get('model').filterBy('isCompleted', false).get('length');
-      }),
-
-      inflection: Ember.computed('remaining', function(){
-      	var remaining = this.get('remaining');
-      	return remaining <= 1 ? 'item' : 'items';
-      }),
-
-      allAreDone: Ember.computed("model.@each.isCompleted", {
-        get() {
-            return this.get('model').get('length') && this.get('model').isEvery('isCompleted');
-      },
-
-      set(key, value) {
-            this.get('model').setEach('isCompleted', value);
-            this.get('model').invoke('save');
-            return value;
-      }
+  set(key, value) {
+    this.get('model').setEach('isCompleted', value);
+    this.get('model').invoke('save');
+    return value;
+  }
 })
-
-
 
 });
--- app/controllers/todos/index.js
+++ app/controllers/todos/index.js
@@ -9,23 +9,23 @@ export default Ember.Controller.extend({
 			todo.destroyRecord();
 		}
 	},
+
 	atComplete: Ember.computed('model.@each.isCompleted', function(){
 		this.get('model').set('createdAt', moment());
 		this.get('model').save();
 		return moment().format('hh:mm:ss');
 	}),
-	isEditing: false,
-	isCompleted: Ember.computed('model.isCompleted', function(key, value){
-		var model = this.get('model');
 
-		if (value === undefined) {
-      // property being used as a getter
-      return model.get('isCompleted');
-  } else {
-      // property being used as a setter
-      model.set('isCompleted', value);
+	isEditing: false,
+
+	isCompleted: Ember.computed('model.isCompleted', function(key, value){
+	  var model = this.get('model');
+	  if (value === undefined) {
+      return model.get('isCompleted'); // property being used as a getter
+    } else {
+      model.set('isCompleted', value); // property being used as a setter
       model.save();
       return value;
-  }
+    }
 })
 });
